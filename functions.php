01f784d2cd4a1e79594bdae29eaf2bf77bdf7fba
SuzukiKaai <suzy092@gmail.com>
2016-04-26 19:39:17 +0800

    isSignin(ログイン処理)関数化

--- functions.php
+++ functions.php
@@ -1,13 +1,37 @@
-<?php 
-    // 全体で頻繁に使用する関数
+<?php
+	// 全体で頻繁に使用する関数
 
-    // htmlspecialcharsのショートカット
-    function h($value) {
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-    }
+	// htmlspecialcharsのショートカット
+	function h($val){
+		return htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
+	}
 
-    // mysqli_real_escape_stringのショートカット
-    function m($db, $value) {
-        return mysqli_real_escape_string($db, $value);
-    }
+	// mysqli_real_escape_stringのショートカット
+	function m($db, $val){
+		return mysqli_real_escape_string($db, $val);
+	}
+
+  // ログイン処理のショートカット
+	function isSignin(){
+		session_start();
+		require('dbconnect.php');
+		if($_SESSION['time'] + 3600 > time()){
+			if(!empty($_SESSION)){
+				//ログインしている場合
+				//「現在日時」を取得
+				$_SESSION['time'] = time();
+				//自分のデータ取得
+				$sql = sprintf('SELECT * FROM members WHERE id=%d',
+								m($db, $_SESSION['id'])
+							);
+				$record = mysqli_query($db, $sql) or die(mysqli_error($db));
+				$member = mysqli_fetch_assoc($record);
+			}
+		}else{
+			// ログインしていない場合：signinへ強制遷移
+			header('Location: signin.php');
+			exit();
+		}
+	}
+
 ?>

a253bc6b1f7f47da70102cf02902d9939a5031e8
tomomifujiwara <the.die.is.cast.19820706@gmail.com>
2016-04-26 09:21:36 +0800

    コード訂正・error解決・コメント一部削除 及びインデント訂正

--- delete.php
+++ delete.php
@@ -1,41 +1,41 @@
 <?php
-  session_start();
-  require('dbconnect.php');
-  require('functions.php');
+    session_start();
+    require('dbconnect.php');
+    require('functions.php');
 
-  // 仮のログインユーザーデータ
-  $_SESSION['id'] = 1;
+    // 仮のログインユーザーデータ
+    $_SESSION['id'] = 1;
 
-      // ユーザーがログインしているかどうか
-      if (isset($_SESSION['id'])) {
-          $id = $_REQUEST['id'];
+        // ユーザーがログインしているかどうか
+        if (isset($_SESSION['id'])) {
+            $id = $_REQUEST['id'];
 
-          $id = array('1' => 'hogehoge','2' => 'fugafu','3' => 'mogo');
-          var_dump($id);
-
-
-          // 投稿があるか確認
-          $sql = sprintf('SELECT * FROM photos WHERE id=%d', m($db , $id)
-          );
-
-          $record = mysqli_query($db, $sql) or die(mysqli_error($db)
-          );
-
-          // 削除したいphotosのデータ一件が格納されている
-          $table = mysqli_fetch_assoc($record);
-
-          // 削除したいphotosデータのmember_idと、ログインしているユーザーのidが一致していれば削除処理実行
-          if ($table['member_id'] == $_SESSION['id']){
-              $sql = sprintf('DELETE FROM phptos WHERE id=%d', m($db, $id)
-          );
-              mysqli_query($db, $sql) or die(mysqli_error($db));
-          }
-      }
+            $id = array('1' => 'hogehoge','2' => 'fugafu','3' => 'mogo');
+            var_dump($id);
 
 
-  // 削除完了後signin.phpへ遷移
-  // header('Location: signin.php');
-  // exit();
+            // 投稿があるか確認
+            $sql = sprintf('SELECT * FROM photos WHERE id=%d', m($db , $id)
+            );
 
-?>
+            $record = mysqli_query($db, $sql) or die(mysqli_error($db)
+            );
+
+            // 削除したいphotosのデータ一件が格納されている
+            $table = mysqli_fetch_assoc($record);
+
+            // 削除したいphotosデータのmember_idと、ログインしているユーザーのidが一致していれば削除処理実行
+            if ($table['member_id'] == $_SESSION['id']){
+                $sql = sprintf('DELETE FROM phptos WHERE id=%d', m($db, $id)
+            );
+                mysqli_query($db, $sql) or die(mysqli_error($db));
+            }
+        }
+
+
+    // 削除完了後signin.phpへ遷移
+    // header('Location: signin.php');
+    // exit();
+
+  ?>
 

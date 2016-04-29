a7b59cabf723df6da0b2956c4edc9d670d8d9bab
tomomifujiwara <the.die.is.cast.19820706@gmail.com>
2016-04-29 10:19:47 +0800

    phpto保存ファイル変更に伴いコード訂正

--- new.php
+++ new.php
@@ -42,8 +42,9 @@
 
         // エラーがなければ
         if(empty($error)){
+            echo 'hoge';
             $image = date('YmdHis') . $_FILES['image']['name'];
-            move_uploaded_file($_FILES['image']['tmp_name'],'./new_pic/' . $image);
+            move_uploaded_file($_FILES['image']['tmp_name'],'vote_photo/' . $image);
             $sql = sprintf('INSERT INTO photos SET photo_path="%s" , title="%s" , comment="%s" , member_id="%d", created=NOW()',
                 $image,
                 m($db, $_POST['title']),
@@ -53,9 +54,8 @@
             );
             mysqli_query($db, $sql) or die(mysqli_error($db));
 
-            unset($_SESSION['join']);
-            header('Location: index.php');
-            exit();
+            // header('Location: index.php');
+            // exit();
         }
     }
 ?>
@@ -75,69 +75,7 @@
   ===================================================================
   ヘッダー
   -->
-  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="index.php">
-          <i class="fa fa-camera-retro fa-1x fa-spin"></i>
-        </a>
-        <a href="index.php" class="navbar-brand">Photo vote</a>
-      </div>
-      <div class="collapse navbar-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="new.php">新規投稿</a></li>
-          <li><a href="users/index.php?id=<?php echo h($_SESSION['id']); ?> " >会員情報</a></li>
-        </ul>
 
-        <ul class="nav navbar-nav navbar-right">
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <span id="heder_p_icon"><img src="profile_image/<?php echo h($member['picture_path']); ?>"></span> 
-              <strong><?php echo h($member['nick_name']); ?>さん</strong>
-              <span class="glyphicon glyphicon-chevron-down"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <div class="navbar-login">
-                  <div class="row">
-                    <div class="col-lg-4">
-                      <p class="text-center">
-                        <span><img class="profile_picture" src="profile_image/<?php echo h($member['picture_path']); ?>"></span>
-                      </p>
-                    </div>
-                    <div class="col-lg-8">
-                      <p class="text-left"><strong><?php echo h($member['nick_name']); ?></strong></p>
-                      <p class="text-left small"><?php echo h($member['email']); ?></p>
-                      <p class="text-left">
-                        <a href="users/index.php?=<?php echo h($_SESSION['id']); ?>" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li class="divider"></li>
-              <li>
-                <div class="navbar-login navbar-login-session">
-                  <div class="row">
-                    <div class="col-lg-12">
-                      <p>
-                        <a href="logout.php" class="btn btn-danger btn-block">ログアウト</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
 
   <!--
   ===================================================================

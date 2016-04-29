ba68aa8f313d67308128bf4214ed700803b4a6f6
tomomifujiwara <the.die.is.cast.19820706@gmail.com>
2016-04-29 09:41:42 +0800

    css完成・実装済・コメントアウトも減らしました

--- new.php
+++ new.php
@@ -14,11 +14,9 @@
             m($db, $_SESSION['id'])
         );
         $record = mysqli_query($db, $sql) or die (mysqli_error($db));
-
-        // ログインしているユーザーのデータ
         $member = mysqli_fetch_assoc($record);
     } else {
-          //ログインしてない
+          //未ログイン
           header('Location; login.php');
           exit();
     }
@@ -37,7 +35,6 @@
         $fileName = $_FILES['image']['name'];
         if(!empty($fileName)) {
             $ext = substr($fileName, -4);
-
             if ($ext != '.jpg' && $ext != '.JPG' && $ext != 'jpeg' && $ext != 'JPEG') {
                 $error['image'] = 'type';
             }
@@ -47,7 +44,6 @@
         if(empty($error)){
             $image = date('YmdHis') . $_FILES['image']['name'];
             move_uploaded_file($_FILES['image']['tmp_name'],'./new_pic/' . $image);
-
             $sql = sprintf('INSERT INTO photos SET photo_path="%s" , title="%s" , comment="%s" , member_id="%d", created=NOW()',
                 $image,
                 m($db, $_POST['title']),
@@ -57,7 +53,7 @@
             );
             mysqli_query($db, $sql) or die(mysqli_error($db));
 
-            //unset($_SESSION['join']);
+            unset($_SESSION['join']);
             header('Location: index.php');
             exit();
         }
@@ -70,35 +66,39 @@
     <meta charset="UTF-8">
     <title>Photovote</title>
     <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css">
-    <!-- ↑bootstrapの読み込み宣言を先にする -->
     <link rel="stylesheet" type="text/css" href="./assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="./assets/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/font-awesome/css/font-awesome.css">
   </head>
   <body>
-    <!--
-    ===================================================================
-    ヘッダー
-    -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a href="index.php" class="navbar-brand">Photo vote</a>
-        </div>
+  <!--
+  ===================================================================
+  ヘッダー
+  -->
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">
+          <i class="fa fa-camera-retro fa-1x fa-spin"></i>
+        </a>
+        <a href="index.php" class="navbar-brand">Photo vote</a>
+      </div>
       <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a href="new.php">新規投稿</a></li>
-          <li><a href="users/index.php?id=<?php echo h($member['id']); ?>" >会員一覧</a></li>
+          <li><a href="users/index.php?id=<?php echo h($_SESSION['id']); ?> " >会員情報</a></li>
         </ul>
+
         <ul class="nav navbar-nav navbar-right">
-  <!--  ↑bootstrapでは、右端に寄せるクラス-->
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <span id="heder_p_icon"><img src="profile_image/<?php echo h($member['picture_path']) ?>"></span> 
-              <strong><?php echo h($member['nick_name']); ?></strong>
+              <span id="heder_p_icon"><img src="profile_image/<?php echo h($member['picture_path']); ?>"></span> 
+              <strong><?php echo h($member['nick_name']); ?>さん</strong>
               <span class="glyphicon glyphicon-chevron-down"></span>
             </a>
             <ul class="dropdown-menu">
@@ -107,138 +107,114 @@
                   <div class="row">
                     <div class="col-lg-4">
                       <p class="text-center">
-                        <span class="glyphicon glyphicon-user icon-size"></span>
+                        <span><img class="profile_picture" src="profile_image/<?php echo h($member['picture_path']); ?>"></span>
                       </p>
                     </div>
                     <div class="col-lg-8">
-                      <p class="text-left"><strong>nick_name</strong></p>
-                      <p class="text-left small">email</p>
+                      <p class="text-left"><strong><?php echo h($member['nick_name']); ?></strong></p>
+                      <p class="text-left small"><?php echo h($member['email']); ?></p>
                       <p class="text-left">
-                        <a href="#" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
+                        <a href="users/index.php?=<?php echo h($_SESSION['id']); ?>" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
                       </p>
                     </div>
                   </div>
                 </div>
               </li>
               <li class="divider"></li>
-                <li>
-                  <div class="navbar-login navbar-login-session">
-                    <div class="row">
-                      <div class="col-lg-12">
-                        <p>
-                        <a href="#" class="btn btn-danger btn-block">ログアウト</a>
-                        </p>
-                      </div>
+              <li>
+                <div class="navbar-login navbar-login-session">
+                  <div class="row">
+                    <div class="col-lg-12">
+                      <p>
+                        <a href="logout.php" class="btn btn-danger btn-block">ログアウト</a>
+                      </p>
                     </div>
                   </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </div>
-    <!--
-    ===================================================================
-    コンテンツ
-    -->
-    <div class="container">
-    <div class="panel panel-default">
-      <div class="panel-heading"><strong>新規投稿</strong>
-        <small>&nbsp;&nbsp;あなたのベストショットを投稿しよう！</small>
-      </div>
-      <form action="" method="post" enctype="multipart/form-data">
-
-      <div class="panel-body">
-        <h5>写真をアップロードする<span class="required">*</span></h5>
-        <dd>
-          <input type="file" name="image">
-            <?php if(!empty($error['image'])): ?>
-              <?php if($error['image'] == 'blank'): ?>
-                <p class="error">写真の選択は必須です。</p>
-              <?php endif; ?>
-
-              <?php if($error['image'] == 'type'): ?>
-                <p class="error">jpgで指定してください。</p>
-              <?php endif; ?>
-            <?php endif; ?>
-        </dd>
-      </div>
-
-      <br>
-
-      <div class="control-group">
-        <h5>タイトルを入力してください。<span class="required">*</span></h5>
-        <div class="controls">
-          <dd>
-            <?php if(!empty($_POST['title'])): ?>
-              <input type="text" name="title" value="<?php echo h($_POST['title'], ENT_QUOTES, 'UTF-8'); ?>">
-            <?php else: ?>
-              <input type="text" name="title" value="">
-            <?php endif; ?>
-
-            <!-- phpでエラー内容出力 -->
-            <?php if(!empty($error['title'])): ?>
-              <?php if ($error['title'] == 'blank'): ?>
-                <p class="error">タイトルを入力してください。</p>
-              <?php endif; ?>
-            <?php endif; ?>
-          </dd>
-        </div>
-      </div>
-
-      <br>
-
-      <div class="control-group">
-        <h5>写真に関するエピソードを書いてみんなにアピールしよう！<span class="required">*</span></h5>
-        <div class="controls">
-          <dd>
-            <?php if(!empty($_POST['comment'])): ?>
-              <textarea name="comment" cols="40" rows="4"><?php echo h($_POST['comment'], ENT_QUOTES, 'UTF-8'); ?></textarea>
-            <?php else: ?>
-              <textarea name="comment" cols="40" rows="4"></textarea>
-            <?php endif; ?>
-
-            <!-- phpでエラー内容出力 -->
-            <?php if(!empty($error['comment'])): ?>
-              <?php if ($error['comment'] == 'blank'): ?>
-                <p class="error">コメントを入力してください。</p>
-              <?php endif; ?>
-            <?php endif; ?>
-          </dd>
-        </div>
-      </div>
-
-      <br>
-
-      <div>
-        <input type="submit" class="btn btn-sm btn-primary" value="投稿完了">
-      </div>
-    </form>
+  </div>
 
   <!--
-    ===================================================================
-    フッター
-    -->
-    <div class="container">
-      <div class="row">
-      <hr>
-        <div class="col-lg-12">
-          <div class="col-md-8">
-            <a href="#">Terms of Service</a> | <a href="#">Privacy</a>
+  ===================================================================
+  コンテンツ
+  -->
+
+  <div class="container">
+    <div class="panel panel-default">
+      <div class="panel-heading"><strong>新規投稿</strong>
+        <small>&nbsp;&nbsp;&nbsp;&nbsp;あなたのベストショットを投稿しよう！</small>
+      </div>
+      <form action="" method="post" enctype="multipart/form-data">
+        <div class="panel-body">
+          <h5>写真をアップロードしてください。<span class="required">*</span></h5>
+          <input type="file" name="image">
+          <?php if(!empty($error['image'])): ?>
+            <?php if($error['image'] == 'blank'): ?>
+              <p class="required">選択されていません</p>
+            <?php endif; ?>
+            <?php if($error['image'] == 'type'): ?>
+              <p class="required">jpgで指定してください</p>
+            <?php endif; ?>
+          <?php endif; ?>
+          <br>
+          <div class="control-group">
+            <h5>投稿タイトルを入力してください。<span class="required">*</span></h5>
+            <div class="controls">
+              <?php if(!empty($_POST['title'])): ?>
+                <input type="text" name="title" value="<?php echo h($_POST['title']); ?>">
+              <?php else: ?>
+                <input type="text" name="title" value="">
+              <?php endif; ?>
+              <!-- phpでエラー内容出力 -->
+              <?php if(!empty($error['title'])): ?>
+                <?php if($error['title'] == 'blank'): ?>
+                  <p class="required">投稿タイトルを入力してください。</p>
+                <?php endif; ?>
+              <?php endif; ?>
+            </div>
           </div>
-          <div class="col-md-4">
-            <p class="muted pull-right">© 2016 Company Name. All rights reserved</p>
+          <br>
+          <div class="control-group">
+            <h5>写真に関するエピソードを書いてみんなにアピールしよう！<span class="required">*</span></h5>
           </div>
+          <div class="controls">
+            <?php if(!empty($_POST['comment'])): ?>
+              <textarea class="photo_message" name="comment" value="<?php echo h($_POST['comment']); ?>"></textarea>
+            <?php else: ?>
+              <textarea type="text" class="photo_message" name="comment"></textarea>
+            <?php endif; ?>
+            <?php if(!empty($error['comment'])): ?>
+              <?php if($error['comment'] == 'blank'): ?>
+                <p class="required">エピソードを入力してください。</p>
+              <?php endif; ?>
+            <?php endif; ?>
+            <input type="submit" class="btn btn-sm btn-primary" id="js-upload-submit" value="投稿する">
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+  <!-- /container -->
+
+  <!--
+  ===================================================================
+  フッター
+  -->
+  <div class="container">
+    <div class="row"><hr>
+      <div class="col-lg-12">
+        <p class="muted_footer">© 2016 AI Matsubara. All rights reserved</p>
         </div>
       </div>
     </div>
-
-    <!-- jsファイルの読み込みはbodyの一番下がデファクトリスタンダード -->
-    <!-- jQueryファイルが一番最初 -->
-    <script type="text/javascript" src="./assets/js/jquery-1.12.3.min.js"></script>
-    <!-- jQueryファイルの次にbootstrapのJSファイル -->
-    <script type="text/javascript" src="./tomomi_aisan2/assets/js/bootstrap.js"></script>
-    <script type="text/javascript" src="./tomomi_aisan2/assets/js/main.js"></script>
-  </body>
-  </html>
+  </div>
+  <script type="text/javascript" src="./assets/js/jquery-1.12.3.min.js"></script>
+  <script type="text/javascript" src="./assets/js/bootstrap.js"></script>
+  <script type="text/javascript" src="./assets/js/main.js"></script>
+</body>
+</html>

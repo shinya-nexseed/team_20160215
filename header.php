f3f2457335da9b4efb69c61f20ff641f4aeac139
SuzukiKaai <suzy092@gmail.com>
2016-05-03 11:36:04 +0800

    [WIP]Header.phpにURLによる条件分岐追加

--- header.php
+++ header.php
@@ -1,42 +1,46 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <title>Photovote</title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.css">
-    <!-- ↑bootstrapの読み込み宣言を先にする -->
-    <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
-    <link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.css">
-  </head>
-  <body>
-    <!--
-    ===================================================================
-    ヘッダー
-    -->
+<?php
+    $url = $_SERVER['REQUEST_URI'];
+?>
+
+    <!--=========================　Header　===========================-->
+    <!--=========================　URLにより条件を分岐させる　===========================-->
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">
+          <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+              <a class="navbar-brand" href="../index.php">
+          <?php else: ?>
+              <a class="navbar-brand" href="index.php">
+          <?php endif; ?>
             <i class="fa fa-camera-retro fa-1x fa-spin"></i>
           </a>
-          <a href="index.php" class="navbar-brand">Photo vote</a>
+          <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+              <a href="../index.php" class="navbar-brand">Photo vote</a>
+          <?php else: ?>
+              <a href="index.php" class="navbar-brand">Photo vote</a>
+          <?php endif; ?>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="new.php">新規投稿</a></li>
-            <li><a href="users/index.php?id=<?php echo h($_SESSION['id']); ?> " >会員情報</a></li>
+              <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+                  <li><a href="../new.php">新規投稿</a></li>
+                  <li><a href="/index.php?id=<?php echo h($_SESSION['id']); ?>">会員情報</a></li>
+              <?php else: ?>
+                  <li><a href="/team20160215/new.php">新規投稿</a></li>
+                  <li><a href="users/index.php?id=<?php echo h($_SESSION['id']); ?>">会員情報</a></li>
+              <?php endif; ?>
           </ul>
           <ul class="nav navbar-nav navbar-right">
             <!--↑bootstrapでは、右端に寄せるクラス-->
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <span id="heder_p_icon"><img src="users/member_picture/<?php echo h($member['picture_path']); ?>"></span> 
+                <span id="heder_p_icon">
+                  <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+                      <img src="member_picture/<?php echo h($member['picture_path']); ?>">
+                  <?php else: ?>
+                      <img src="users/member_picture/<?php echo h($member['picture_path']); ?>">
+                  <?php endif; ?>
+                </span> 
                 <strong><?php echo h($member['nick_name']); ?>さん</strong>
                 <span class="glyphicon glyphicon-chevron-down"></span>
               </a>
@@ -46,7 +50,11 @@
                     <div class="row">
                       <div class="col-lg-4">
                         <p class="text-center">
-                          <span><img class="profile_picture" src="users/member_picture/<?php echo h($member['picture_path']); ?>"></span>
+                          <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+                              <span><img class="profile_picture" src="member_picture/<?php echo h($member['picture_path']); ?>"></span>
+                          <?php else: ?>
+                              <span><img class="profile_picture" src="users/member_picture/<?php echo h($member['picture_path']); ?>"></span>
+                          <?php endif; ?>
                         </p>
                       </div>
                       <div class="col-lg-8">
@@ -65,7 +73,11 @@
                     <div class="navbar-login navbar-login-session">
                       <div class="row">
                         <div class="col-lg-12">
-                          <p><a href="signout.php" class="btn btn-danger btn-block">サインアウト</a></p>
+                          <?php if($url=="users/index.php"||"users/edit.php"||"users/setting.php"||"users/view.php"): ?>
+                              <p><a href="../signout.php" class="btn btn-danger btn-block">サインアウト</a></p>
+                          <?php else: ?>
+                              <p><a href="signout.php" class="btn btn-danger btn-block">サインアウト</a></p>
+                          <?php endif; ?>
                         </div>
                       </div>
                     </div>
@@ -77,5 +89,3 @@
         </div>
       </div>
     </div>
-  </body>
-</html>

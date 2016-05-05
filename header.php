4bdce74e2d371f130d16b239a94484fc0e1e3a46
shinya-nexseed <shirai@nexseed.net>
2016-05-06 02:00:12 +0800

    分岐文修正

--- header.php
+++ header.php
@@ -13,13 +13,13 @@
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+          <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
               <a class="navbar-brand" href="../index.php"><i class="fa fa-camera-retro fa-1x fa-spin"></i></a>
           <?php else: ?>
               <a class="navbar-brand" href="index.php"><i class="fa fa-camera-retro fa-1x fa-spin"></i></a>
           <?php endif; ?>
           <!-- 下記の$urlとREQUEST_URI内にある$url(var_dumpしたもの)が一致していること -->
-          <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+          <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
               <a href="../index.php" class="navbar-brand">Photo vote</a>
           <?php else: ?>
               <a href="index.php" class="navbar-brand">Photo vote</a>
@@ -27,7 +27,7 @@
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-              <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+              <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
                   <li><a href="../rank.php" >ランキング</a></li>
                   <li><a href="../new.php">新規投稿</a></li>
                   <li><a href="../users/index.php">会員情報</a></li>
@@ -42,7 +42,7 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <span id="heder_p_icon">
-                  <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+                  <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
                       <img src="member_picture/<?php echo h($member['picture_path']); ?>">
                   <?php else: ?>
                       <img src="users/member_picture/<?php echo h($member['picture_path']); ?>">
@@ -57,7 +57,7 @@
                     <div class="row">
                       <div class="col-lg-4">
                         <p class="text-center">
-                          <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+                          <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
                               <span><img class="profile_picture" src="member_picture/<?php echo h($member['picture_path']); ?>"></span>
                           <?php else: ?>
                               <span><img class="profile_picture" src="users/member_picture/<?php echo h($member['picture_path']); ?>"></span>
@@ -68,10 +68,10 @@
                         <p class="text-left"><strong><?php echo h($member['nick_name']); ?></strong></p>
                         <p class="text-left small"><?php echo h($member['email']); ?></p>
                         <p class="text-left">
-                          <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
-                              <a href="index.php" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
+                          <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
+                              <a href="view.php?id=<?php echo $member['id']; ?>" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
                           <?php else: ?>
-                              <a href="users/index.php" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
+                              <a href="users/view.php?id=<?php echo $member['id']; ?>" class="btn btn-primary btn-block btn-sm">マイプロフィール</a>
                           <?php endif; ?>
                         </p>
                       </div>
@@ -83,7 +83,7 @@
                     <div class="navbar-login navbar-login-session">
                       <div class="row">
                         <div class="col-lg-12">
-                          <?php if($url=="/team_20160215/users/index.php"||$url=="/team_20160215/users/edit.php"||$url=="/team_20160215/users/view.php"||$url=="/team_20160215/users/setting.php"): ?>
+                          <?php if(strpos($url,'/team_20160215/users/index.php') !== false || strpos($url,'/team_20160215/users/edit.php') !== false || strpos($url,'/team_20160215/users/view.php') !== false || strpos($url,'/team_20160215/users/setting.php') !== false): ?>
                               <p><a href="../signout.php" class="btn btn-danger btn-block">サインアウト</a></p>
                           <?php else: ?>
                               <p><a href="signout.php" class="btn btn-danger btn-block">サインアウト</a></p>
